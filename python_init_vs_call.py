a3f36ff8333497de245182b255751817871fe808
Prabhath Kota <prabhath.kota@rackspace.com>
2019-05-30 14:30:43 +0530

    Python __init__ vs __call__

--- python_init_vs_call.py
+++ python_init_vs_call.py
@@ -11,17 +11,40 @@ class Foo:
         self.a = a
         self.b = b
         self.c = c
+        print('In Foo __init__ {}, {}, {}'.format(self.a, self.b, self.c))
 
-    def __call__(self, a, b, c):
+
+class Bar:
+    def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
+    def __call__(self):
+        print('In Bar __call__ {}, {}, {}' .format(self.a, self.b, self.c))
+
 
 if __name__ == '__main__':
-    #f = Foo(100, 200, 300)
-    #print(f.a)
-    f1 = Foo()
-    f1(100, 200, 300)
-    print(f1.c)
+    print('--------------------------------------------------')
+    f = Foo(100, 200, 300)
+    print(f.a)
+    #f()    #'Foo' object is not callable
+    print('--------------------------------------------------')
+    b = Bar(10, 20, 30)
+    print(b.a)
+    b()
+    print('--------------------------------------------------')
+
+
+"""
+Output:
+
+--------------------------------------------------
+In Foo __init__ 100, 200, 300
+100
+--------------------------------------------------
+10
+In Bar __call__ 10, 20, 30
+--------------------------------------------------
+"""
 
